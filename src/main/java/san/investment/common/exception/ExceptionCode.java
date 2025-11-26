dbd2465e9602a0587198c122467876d4911a4d5b
jwlee <jwlee@unipost.co.kr>
2025-11-26 18:07:09 +0900

    에러코드 추가

--- src/main/java/san/investment/common/exception/ExceptionCode.java
+++ src/main/java/san/investment/common/exception/ExceptionCode.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
 
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "-1", "서버에러"),
-
+    EXPIRE_TOKEN(HttpStatus.UNAUTHORIZED, "-2", "토큰이 만료 되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "-3", "유효하지 않은 토큰입니다."),
     // 로그인 관련
-    INVALID_USER_ID(HttpStatus.UNAUTHORIZED,"-100", "유효하지 않은 사용자 ID 입니다.")
+    INVALID_USER_ID(HttpStatus.UNAUTHORIZED,"-100", "유효하지 않은 사용자 ID 입니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "-101", "비밀번호가 일치하지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
