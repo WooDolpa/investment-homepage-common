ccb2407cf5ccf391d49765ba3c1225fbbaba0ffc
jwlee <dannis1124@naver.com>
2025-12-21 23:12:39 +0900

    회사 조회 관련 에러 코드 추가

--- src/main/java/san/investment/common/exception/ExceptionCode.java
+++ src/main/java/san/investment/common/exception/ExceptionCode.java
@@ -15,7 +15,10 @@ public enum ExceptionCode {
     EXPIRE_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "-5", "리플래쉬 토큰이 만료 되었습니다."),
     // 로그인 관련
     INVALID_USER_ID(HttpStatus.UNAUTHORIZED,"-100", "유효하지 않은 사용자 ID 입니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "-101", "비밀번호가 일치하지 않습니다.")
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "-101", "비밀번호가 일치하지 않습니다."),
+
+    // 회사 관련
+    NOT_FOUND_COMPANY(HttpStatus.NOT_FOUND, "-110", "존재하지 않는 회사 아이디입니다.")
     ;
 
     private final HttpStatus httpStatus;
