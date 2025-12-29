c3a2dd61175c951979db2901e12877f1b27bb127
jwlee <dannis1124@naver.com>
2025-12-30 08:57:22 +0900

    포트폴리오 메인노출 엔티티 추가

---
+++ src/main/java/san/investment/common/entity/portfolio/PortfolioMain.java
@@ -0,0 +1,45 @@
+package san.investment.common.entity.portfolio;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import san.investment.common.entity.BaseEntity;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+/**
+ * packageName : san.investment.common.entity.portfolio
+ * className : PortfolioMain
+ * user : jwlee
+ * date : 2025. 12. 29.
+ * description :
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "portfolio_main")
+public class PortfolioMain extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer portfolioMainNo;
+
+    @Column(name = "order_num")
+    @Comment("순번")
+    private Integer orderNum;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "portfolio_no")
+    private Portfolio portfolio;
+
+    @Builder
+    public PortfolioMain(Integer portfolioMainNo, Integer orderNum, Portfolio portfolio) {
+        this.portfolioMainNo = portfolioMainNo;
+        this.orderNum = orderNum;
+        this.portfolio = portfolio;
+    }
+}
