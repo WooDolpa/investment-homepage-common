fd0668565a402d13e66e6a8ed1f104e10b2503ed
jwlee <dannis1124@naver.com>
2026-05-31 15:31:12 +0900

    update

--- src/main/java/san/investment/common/exception/CustomExceptionHandler.java
+++ src/main/java/san/investment/common/exception/CustomExceptionHandler.java
@@ -1,6 +1,8 @@
 package san.investment.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import san.investment.common.dto.ApiResponseDto;
@@ -11,5 +13,11 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handleException(CustomException e) {
         return new ResponseEntity<>(ApiResponseDto.makeResponse(e), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
+        CustomException customException = new CustomException(ExceptionCode.INVALID_PARAMETER);
+        return new ResponseEntity<>(ApiResponseDto.makeResponse(customException), HttpStatus.BAD_REQUEST);
     }
 }
